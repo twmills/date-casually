d469fbb37cc44849e9b0dbd43cc5388ac008c940
Theo Mills <twmills@gmail.com>
2010-09-03 11:30:14 -0500

    Updated documentation and changed from a class to a module. Tomdoc FTW!

--- lib/date-casually/config.rb
+++ lib/date-casually/config.rb
@@ -1,14 +1,55 @@
 module DateCasually  
-  class Config
+  
+  # Contains global configuration options for DateCasually. These options 
+  # may be changed globally at runtime by using the designated class method. 
+  # For example:
+  #
+  #   DateCasually::Config.as :years
+  #   (Date.today + 2).casual
+  #   #=> "less than a year from now"
+  #
+  module Config
+    
     class << self
+      
+      # Gets and sets a map that tells DateCasually which translation options to use 
+      # when translating the current date. Values should be passed as an array of symbols.
+      # Possible values are :day, :week, :month, :year, :day_of_week. 
+      #
+      # Examples:
+      #
+      #   date = Date.new(2010, 8, 30)
+      #
+      #   (date + 6).casual(:day_of_week)
+      #   #=> 'next week'
+      #
+      #   (date + 1).casual(:months) 
+      #   #=> 'less than a month from now'
+      #
+      #   (date + 660).casual(:months)
+      #   #=> '21 months from now'
+      #
+      #   (date + 660).casual(:months, :years)
+      #   #=> 'a couple of years from now'      
+      #
       attr_accessor :as
     end
     
+    # Resets the classes configuration options back to their defaults. Used
+    # primarily to reset the class configuration options when the module 
+    # gets loaded.
+    #
+    # Example:
+    #
+    #   self.reset
+    #
+    # Returns nothing.
     def self.reset
       self.as = [:days, :weeks, :months, :years]
     end
     
-    #reset the first time we are loaded.
+    # Reset the first time we are loaded.
     self.reset
+    
   end
 end
